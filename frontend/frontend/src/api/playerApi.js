d5b2dc308c2bfceee285ad28f852e18f3a91cf61
Nishant Kshirsagar <90412779+Ni-vik@users.noreply.github.com>
2025-04-28 01:31:14 +0530

    Update playerApi.js

--- frontend/frontend/src/api/playerApi.js
+++ frontend/frontend/src/api/playerApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/players'; // backend base url
+const API_URL = 'https://iplguesser.onrender.com/api/players'; // backend base url
 
 export const getRandomPlayer = async () => {
   const { data } = await axios.get(`${API_URL}/random`);
