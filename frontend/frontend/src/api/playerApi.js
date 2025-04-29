19063ef902d89d389483ddf943f56f5b37cd7087
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2025-04-29 13:47:17 +0530

    Update playerApi.js

--- frontend/frontend/src/api/playerApi.js
+++ frontend/frontend/src/api/playerApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://iplguesser.onrender.com/api/players'; // backend base url
+const API_URL = 'https://iplguesser.onrender.com/api/players'; 
 
 export const getRandomPlayer = async () => {
   const { data } = await axios.get(`${API_URL}/random`);
@@ -11,3 +11,8 @@ export const checkPlayerGuess = async (playerId, guess) => {
   const { data } = await axios.post(`${API_URL}/guess`, { playerId, guess });
   return data;
 };
+
+export const getHint = async (p_name) => {
+  const { data } = await axios.post(`${API_URL}/hint`,{p_name});
+  return data;
+}
