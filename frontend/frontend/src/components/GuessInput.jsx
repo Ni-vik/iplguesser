d28edaac327f0953547b84e17ff99e2f9329b5b6
ni-vik <nishantkshirsagar135@gmail.com>
2025-05-06 20:52:58 +0530

    CSS improvements

--- frontend/frontend/src/components/GuessInput.jsx
+++ frontend/frontend/src/components/GuessInput.jsx
@@ -1,12 +1,10 @@
 const GuessInput = ({ guess, setGuess, handleGuess }) => {
     return (
       <div className="flex items-center space-x-2 mt-4">
-        <input
-          type="text"
+        <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Guess the Player..."
           value={guess}
           onChange={(e) => setGuess(e.target.value)}
-          className="border p-2 rounded-md w-64"
         />
         <button
           onClick={handleGuess}
