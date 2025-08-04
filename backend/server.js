042a199dcb9c9c7be9bd826e013993dfc60a9fb3
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2025-08-04 15:16:48 +0530

    Added uptime check

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/players', playerRoutes);
-
+app.get('/health-check', (req, res) => {
+  res.status(200).send('OK');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
