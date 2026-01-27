1c58e1837e391481fd54d1fd1df6c62119bb65d4
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2026-01-27 15:24:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ import playerRoutes from './routes/playerRoutes.js';
 dotenv.config();
 connectDB();
 
+const corsorigin = {
+  origin : 'https://guesstheplayer.onrender.com/'
+}
+
 const app = express();
-app.use(cors());
+app.use(cors(corsorigin));
 app.use(express.json());
 
 app.use('/api/players', playerRoutes);
-app.get('/health-check', (req, res) => {
-  res.status(200).send('OK');
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
